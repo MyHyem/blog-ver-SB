e711d28ff8ee13f5fbf43a9ee04a375a31e2bbd9
Sangjae <hellohj@counpia.com>
2021-08-18 14:45:54 +0900

    버전업

--- src/main/java/com/portfolio/blog/common/ui/ExceptionController.java
+++ src/main/java/com/portfolio/blog/common/ui/ExceptionController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.security.Principal;
+
 @Controller
 public class ExceptionController {
 
